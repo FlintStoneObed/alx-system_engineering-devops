11f6628d78875b38334bab1dae852024aaa79d13
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2024-04-19 08:17:58 -0700

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-#!/usr/bin/pup
-# installs the package puppet-lint
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package {'flask':
-	ensure	 => '2.1.0',
-	provider => 'pip'
+# Install Flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
